f7e245da9c5459e04583993063bfd1a67f2156e0
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-08-23 17:04:39 +0330

    INIT

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,10 +7,7 @@ const NotFound = () => {
   return (
     <div className={`${notFoundBackground} w-full`}>
       <div className="flex flex-col justify-center items-center h-[58vh] w-[50vh] mx-auto">
-        <p
-          className="text-white font-normal font-faNum lg:text-[27vh] text-[27vh]"
-          style={{ textShadow: "-15px 14px 2px #79A4FF" }}
-        >
+        <p className="text-white font-normal font-faNum lg:text-[27vh] text-[27vh]" style={{ textShadow: "-15px 14px 2px #79A4FF" }}>
           404
         </p>
         <Link href="/" className="text-[5vh] text-center font-bold font-YekanBakh bg-[#EC6A00] text-white rounded-full py-3 w-full">
