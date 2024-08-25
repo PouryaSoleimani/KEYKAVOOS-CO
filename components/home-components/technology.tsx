a72e2ecd2036ccdf20387d0a13c7af7524e6b3d1
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-08-25 10:47:37 +0330

    ANIMATION

--- components/home-components/technology.tsx
+++ components/home-components/technology.tsx
@@ -5,7 +5,7 @@ import { TechnologyData } from "@/lib/data";
 
 function Technology() {
   return (
-    <div className="flex flex-col" data-aos="fade-up" data-aos-duration="1500">
+    <div className="flex flex-col mt-8" data-aos="fade-up" data-aos-duration="1500">
       <SectionHeader mainTitle="تکنولوژی های طراحی و راه اندازی سایت در کیکاووس" subTitle="کدام طرح مناسب کسب و کار شماست؟" width="20%" />
       <div className="grid grid-cols-1 gap-5 md:max-lg:grid-cols-2 md:max-lg:gap-20 lg:grid-cols-3 lg:gap-x-32">
         {TechnologyData.map((item) => (
