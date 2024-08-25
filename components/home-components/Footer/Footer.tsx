e85facda88f677a3f7ff21c2f8bf8629a5cdafb0
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-08-25 12:00:23 +0330

    HOME PAGE UI

--- components/home-components/Footer/Footer.tsx
+++ components/home-components/Footer/Footer.tsx
@@ -44,12 +44,12 @@ const Contact = () => {
       </div>
 
       <div className="flex flex-row items-center gap-2 mt-1">
-        <Image src={PhoneIcon} alt="phone" className="w-6 h-6 -rotate-12"/>
+        <Image src={PhoneIcon} alt="phone" className="w-6 h-6 -rotate-12" />
         <p className="font-faNum text-[17px] tracking-widest">021-91691650</p>
       </div>
 
       <div className="flex flex-row items-center gap-2 mt-2">
-        <Image src={MailIcon} alt="mail" className="w-6 h-6"/>
+        <Image src={MailIcon} alt="mail" className="w-6 h-6" />
         <p className="text-[17px] tracking-widest">info@keykavoos.co</p>
       </div>
     </div>
@@ -60,18 +60,11 @@ const AuthLogos = () => {
   return (
     <div className="flex flex-col gap-3 col-span-2">
       <SocialMedia />
-      <div className="flex justify-around items-center">
-        <Link
-          referrerPolicy="origin"
-          target="_blank"
-          href="https://trustseal.enamad.ir/?id=422622&Code=KGdskS8BDuEuvxkxW9GT5eCCOJ3TO3sa"
-        >
+      <div className="flex justify-center py-2 items-center">
+        <Link referrerPolicy="origin" target="_blank" href="https://trustseal.enamad.ir/?id=422622&Code=KGdskS8BDuEuvxkxW9GT5eCCOJ3TO3sa">
           <Image src={SenfiLogo} alt="senfi-logo" className="w-[5em]" />
         </Link>
-        <Link
-          href="https://trustseal.enamad.ir/?id=446767&code=Y6oDWjmfcaGlRuWAK8t9YKGUq2VNkzYJ"
-          target="_blank"
-        >
+        <Link href="https://trustseal.enamad.ir/?id=446767&code=Y6oDWjmfcaGlRuWAK8t9YKGUq2VNkzYJ" target="_blank">
           <Image src={EnamaadLogo} alt="enamaad-logo" className="w-[5em]" />
         </Link>
       </div>
