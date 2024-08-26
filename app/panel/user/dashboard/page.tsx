255872ecad27012941c138b87ec7bc84d9f3277f
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-08-26 20:00:36 +0330

    USER PANEL

--- app/panel/user/dashboard/page.tsx
+++ app/panel/user/dashboard/page.tsx
@@ -6,7 +6,7 @@ function Dashboard() {
   return (
     <div className="grid grid-cols-2 max-md:grid-cols-1 max-md:my-5 gap-y-20 justify-center items-center text-center bg-white shadow mx-auto rounded-2xl w-full px-[3%] py-20 lg:py-[8%]">
       {UserDashboardInfo.map((item) => (
-        <DashboardCard key={item.id} data={item}/>
+        <DashboardCard key={item.id} data={item} />
       ))}
     </div>
   );
