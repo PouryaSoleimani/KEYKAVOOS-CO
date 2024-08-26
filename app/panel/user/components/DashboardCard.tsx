8859b012976b91a9e0e2e0adfde28f9f77e81c40
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-08-26 22:05:04 +0330

    REFACTOR

--- app/panel/user/components/DashboardCard.tsx
+++ app/panel/user/components/DashboardCard.tsx
@@ -13,16 +13,9 @@ type DashboardCardProps = {
 };
 function DashboardCard({ data }: DashboardCardProps) {
   return (
-    <Link
-      href={data.link}
-      className="bg-[#DEE5FE] h-[150px] w-[300px] flex justify-center items-center rounded-[20px] mx-auto relative"
-    >
+    <Link href={data.link} className="bg-[#DEE5FE] h-[150px] w-[300px] flex justify-center items-center rounded-[20px] mx-auto relative"  >
       <p className="font-semibold text-[24px]">{data.title}</p>
-      <Image
-        src={data.imgUrl}
-        alt={data.title}
-        className={`absolute ${data.title.includes("وضعیت سفارش") ? "-top-20" : "-top-14"}`}
-        width={data.width}
+      <Image src={data.imgUrl} alt={data.title} className={`absolute ${data.title.includes("وضعیت سفارش") ? "-top-20" : "-top-14"}`} width={data.width}
       />
     </Link>
   );
