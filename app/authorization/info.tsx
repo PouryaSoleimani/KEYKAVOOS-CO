a8b132bd8fc6d5207e5f4a3124aefe653b7f181b
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-08-26 20:14:40 +0330

    USER PANEL

--- app/authorization/info.tsx
+++ app/authorization/info.tsx
@@ -9,10 +9,7 @@ type LoginProps = {
 const Info = ({ setSteps }: LoginProps) => {
   return (
     <React.Fragment>
-      <div
-        className="mx-auto grid grid-cols-1 font-YekanBakh rounded-3xl overflow-hidden my-[3%] shadow-2xl shadow-[13px_0_61px_-24px_rgba(0, 0, 0, 0.15)]"
-        dir="rtl"
-      >
+      <div className="mx-auto grid grid-cols-1 font-YekanBakh rounded-3xl overflow-hidden my-[3%] shadow-2xl shadow-[13px_0_61px_-24px_rgba(0, 0, 0, 0.15)]" dir="rtl" >
         <div className="py-[5%] w-full relative px-[5%]">
           <Logo />
           <InfoForm setSteps={setSteps} />
