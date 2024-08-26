c4b49e28f7f34c64606d006cf6d60bab652099dc
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-08-26 22:13:01 +0330

    REFACTOR

--- app/panel/user/components/DashboardCard.tsx
+++ app/panel/user/components/DashboardCard.tsx
@@ -2,15 +2,8 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-type DashboardCardProps = {
-  data: {
-    id: number;
-    title: string;
-    link: string;
-    imgUrl: string;
-    width: number;
-  };
-};
+type DashboardCardProps = { data: { id: number; title: string; link: string; imgUrl: string; width: number; }; };
+
 function DashboardCard({ data }: DashboardCardProps) {
   return (
     <Link href={data.link} className="bg-[#DEE5FE] h-[150px] w-[300px] flex justify-center items-center rounded-[20px] mx-auto relative"  >
