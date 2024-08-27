6c507169cf577fb95f2f81b9d579d04bad8de21a
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-08-27 23:14:01 +0330

    USER PANEL

--- app/panel/components/panel-fileds.tsx
+++ app/panel/components/panel-fileds.tsx
@@ -1,29 +1,14 @@
 import React from "react";
-type PanelFieldsProps = {
-  label: string;
-  value: string | undefined;
-  onChange?: React.ChangeEventHandler<HTMLInputElement>;
-  disable?: boolean;
-  name?: string;
-  readonly?: boolean;
-  flexDirection?: string;
-};
-function PanelFields({
-  label,
-  onChange,
-  value,
-  disable,
-  name,
-  readonly,
-  flexDirection
-}: PanelFieldsProps) {
+type PanelFieldsProps = { label: string; value: string | undefined; onChange?: React.ChangeEventHandler<HTMLInputElement>; disable?: boolean; name?: string; readonly?: boolean; flexDirection?: string; };
+
+function PanelFields({ label, onChange, value, disable, name, readonly, flexDirection }: PanelFieldsProps) {
+  // ^ RETURN
   return (
     <div className={`flex ${flexDirection ? `${flexDirection} items-center` : "flex-col"}  gap-3`}>
       <label htmlFor="">{label}</label>
       <input
-        className={`${
-          disable ? "bg-[#EAEFF6] text-slate-500 px-3" : "bg-[#EAEFF6] px-2"
-        } outline-none border-none py-2 rounded-[4px]`}
+        className={`${disable ? "bg-[#EAEFF6] text-slate-500 px-3" : "bg-[#EAEFF6] px-2"
+          } outline-none border-none py-2 rounded-[4px]`}
         onChange={onChange}
         value={value}
         disabled={disable}
