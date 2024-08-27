361bc90c99bee50b0b382a0d004a1b66b7085a65
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-08-27 23:19:51 +0330

    USER PANEL

--- app/panel/components/panel-fileds.tsx
+++ app/panel/components/panel-fileds.tsx
@@ -6,8 +6,7 @@ function PanelFields({ label, onChange, value, disable, name, readonly, flexDire
   return (
     <div className={`flex ${flexDirection ? `${flexDirection} items-center` : "flex-col"}  gap-1`}>
       <label htmlFor="" className="tracking-tight">{label}</label>
-      <input
-        className={`${disable ? "bg-[#EAEFF6] text-slate-500 px-3" : "bg-[#EAEFF6] px-2"} translate-x-1 font-extralight tracking-tight outline-none border-none py-2 rounded-[4px]`}
+      <input className={`${disable ? "bg-[#EAEFF6] text-slate-500/60 px-3" : "bg-[#EAEFF6] px-2"} translate-x-1 font-extralight tracking-tight outline-none border-none py-2 rounded-[6px]`}
         onChange={onChange}
         value={value}
         disabled={disable}
