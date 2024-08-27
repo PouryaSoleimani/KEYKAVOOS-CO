490699baf2cb4d2c0bedca6d0c8452735d34ea2c
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-08-27 23:18:09 +0330

    USER PANEL

--- app/panel/components/panel-fileds.tsx
+++ app/panel/components/panel-fileds.tsx
@@ -4,11 +4,10 @@ type PanelFieldsProps = { label: string; value: string | undefined; onChange?: R
 function PanelFields({ label, onChange, value, disable, name, readonly, flexDirection }: PanelFieldsProps) {
   // ^ RETURN
   return (
-    <div className={`flex ${flexDirection ? `${flexDirection} items-center` : "flex-col"}  gap-3`}>
-      <label htmlFor="">{label}</label>
+    <div className={`flex ${flexDirection ? `${flexDirection} items-center` : "flex-col"}  gap-1`}>
+      <label htmlFor="" className="tracking-tight">{label}</label>
       <input
-        className={`${disable ? "bg-[#EAEFF6] text-slate-500 px-3" : "bg-[#EAEFF6] px-2"
-          } outline-none border-none py-2 rounded-[4px]`}
+        className={`${disable ? "bg-[#EAEFF6] text-slate-500 px-3" : "bg-[#EAEFF6] px-2"} translate-x-1 font-extralight tracking-tight outline-none border-none py-2 rounded-[4px]`}
         onChange={onChange}
         value={value}
         disabled={disable}
