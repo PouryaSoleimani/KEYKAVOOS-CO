f972dbb69526b0688611d445b919dbf1a9e580da
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-08-28 20:42:27 +0330

    REFACTOR

--- app/panel/user/components/NewInfoOnEachPageBtn.tsx
+++ app/panel/user/components/NewInfoOnEachPageBtn.tsx
@@ -2,13 +2,9 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import add from "@/public/Panel/addticket.svg";
-const NewInfoOnEachPageBtn = ({
-  btnText,
-  src,
-}: {
-  btnText: string;
-  src: string;
-}) => {
+
+//^ COMPONENT
+const NewInfoOnEachPageBtn = ({ btnText, src, }: { btnText: string; src: string; }) => {
   return (
     <Link href={src} className="text-white bg-[#4866CF] p-2 rounded-[5px] flex flex-row gap-3 items-center hover:bg-blue-800 duration-300 tracking-tight" >
       {btnText}
