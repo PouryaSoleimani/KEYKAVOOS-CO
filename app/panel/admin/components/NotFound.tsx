3e7b8c5135cb8b9a173976042731c85e36b850fb
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-08-28 20:58:53 +0330

    REFACTOR

--- app/panel/admin/components/NotFound.tsx
+++ app/panel/admin/components/NotFound.tsx
@@ -5,7 +5,7 @@ type NotFoundProps = { text: string; };
 
 function NotFound({ text }: NotFoundProps) {
   return (
-    <p className="text-center bg-red-200/80  tracking-tight leading-10 text-lg text-red-700  p-2 rounded-md">
+    <p className="text-center bg-red-100 tracking-tight leading-10 text-lg text-red-700 p-3 mt-8 rounded-md">
       {text}
     </p>
   );
