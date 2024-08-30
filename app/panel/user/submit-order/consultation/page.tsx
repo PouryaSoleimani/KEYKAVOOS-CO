e06bb2de568308723803a2fb1fd5627434401144
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-08-30 16:14:19 +0330

    USER PANEL

--- app/panel/user/submit-order/consultation/page.tsx
+++ app/panel/user/submit-order/consultation/page.tsx
@@ -7,17 +7,14 @@ import TicketIcon from "@/public/Consult/ticket.svg";
 import Link from "next/link";
 function Consultation() {
   return (
-    <div
-      className="py-[3%] w-[100%] shadow mx-auto bg-white rounded-2xl px-[3%] grid grid-cols-1 gap-8"
-      dir="ltr"
-    >
+    <div className="py-[3%] w-[100%] shadow mx-auto bg-white rounded-2xl px-[3%] grid grid-cols-1 gap-8" dir="ltr"  >
       <div className="grid grid-cols-1 gap-5 lg:gap-0 lg:grid-cols-2">
         <div>
           <Image src={ConsultImage} alt="consultation" />
         </div>
         <div className="text-right flex flex-col gap-5">
-          <p className="text-[25px] font-bold">مشاوره رایگان</p>
-          <p className="leading-9 text-[18px] text-justify" dir="rtl">
+          <p className="text-[24px] font-semibold tracking-tight text-zinc-900">مشاوره رایگان</p>
+          <p className="leading-8 text-[16px] font-thin text-zinc-800 text-justify tracking-tight" dir="rtl">
             وقتی ثروت‌ های بزرگ به دست برخی مردم می‌افتد در پرتو آن نیرومند
             می‌شوند و در سایهٔ نیرومندی و ثروت خیال می‌ کنند که می‌توانند در
             خارج از وطن خود زندگی نمایند و خوشبخت و سرافراز باشند ولی به زودی
@@ -27,18 +24,12 @@ function Consultation() {
         </div>
       </div>
 
-      <div
-        className="text-[#4866CE] flex flex-col lg:flex-row justify-between items-center gap-5"
-        dir="rtl"
-      >
-        <div className="border-[2px] border-[#4866CE] bg-[#F9F9F9] p-3 rounded-[12px] flex flex-row items-center justify-between lg:w-[50%] w-full">
+      <div className="text-[#F9F9F9] flex flex-col lg:flex-row justify-between items-center gap-5" dir="rtl"  >
+        <div className="bg-[#4866CF] hover:bg-blue-800 tracking-tight duration-300 p-3 cursor-pointer rounded-[12px] flex flex-row items-center justify-between lg:w-[50%] w-full">
           <p>ارتباط از طریق شماره تماس 91691650</p>
           <Image src={PhoneIcon} alt="phone-icon" width={30} />
         </div>
-        <Link
-          href="/panel/user/submit-order/consultation/submit-consultation"
-          className="border-[2px] border-[#4866CE] bg-[#F9F9F9] p-3 rounded-[12px] flex flex-row items-center justify-between lg:w-[50%] w-full h-full"
-        >
+        <Link href="/panel/user/submit-order/consultation/submit-consultation" className="bg-[#4866CF] hover:bg-blue-800 tracking-tight duration-300 p-4 cursor-pointer rounded-[12px] flex flex-row items-center justify-between lg:w-[50%] w-full" >
           <p>ثبت مشاوره از طریق تیکت</p>
           <Image src={TicketIcon} alt="phone-icon" width={30} />
         </Link>
