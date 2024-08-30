b93525f7f276710fe91479ab46af365bd9c9a769
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-08-30 19:46:32 +0330

    USER PANEL

--- app/panel/user/personal-info/genuine.tsx
+++ app/panel/user/personal-info/genuine.tsx
@@ -18,10 +18,10 @@ function Genuine({ userProfile }: GenuineProps) {
         </div>
         <div className="flex flex-col gap-5">
           <div className="self-center">
-            <Image src={userProfile.pic_path ? `http://localhost:8000/storage/${userProfile.pic_path}` : USER__DEFAULT} alt="profile" width={180} height={1800} />
+            <Image src={userProfile.pic_path ? `http://localhost:8000/storage/${userProfile.pic_path}` : USER__DEFAULT} alt="profile_pic" width={52} height={152} className="flex items-center text-zinc-500"/>
           </div>
           <div className="flex justify-center">
-            <Link href={"/panel/user/settings"} className="bg-[#4866CF] text-white text-center p-2 rounded-lg w-[160px] tracking-tight text-[14px] hover:bg-blue-800 duration-300"  >
+            <Link href={"/panel/user/settings"} className="bg-[#4866CF] text-white text-center px-2 py-3 rounded-lg w-[160px] tracking-tight text-[14px] hover:bg-blue-800 duration-300"  >
               ویرایش حساب کاربری
             </Link>
           </div>
