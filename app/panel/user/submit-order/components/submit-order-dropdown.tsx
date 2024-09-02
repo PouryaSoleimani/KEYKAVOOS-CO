f3f9323e92ba66db97d41e3009c34abea07dc0f7
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-09-02 17:30:56 +0330

    USER PANEL

--- app/panel/user/submit-order/components/submit-order-dropdown.tsx
+++ app/panel/user/submit-order/components/submit-order-dropdown.tsx
@@ -9,7 +9,7 @@ function SubmitOrderDropdown({ dropDownTitle, dropdownItems, value, onChange, na
   return (
     <div className={`flex ${dropDownTitle ? "flex-col gap-1" : "flex-row"} relative`} >
       <p>{dropDownTitle}</p>
-      <select name={name ? name : dropDownTitle} id={value} className={`bg-[#EAEFF6] h-full rounded-[4px] text-zinc-800 font-YekanBakh tracking-tighter font-extralight px-3 py-2 ${!dropDownTitle && "w-full font-YekanBakh"}`} value={`${value}`} onChange={onChange}>
+      <select name={name ? name : dropDownTitle} id={value} className={`bg-[#EAEFF6] h-full rounded-[4px] text-zinc-700 font-YekanBakh tracking-tighter text-md font-extralight px-3 py-2 ${!dropDownTitle && "w-full font-YekanBakh"}`} value={`${value}`} onChange={onChange}>
         {dropdownItems?.map((item) => (
           <option key={item} value={item} className="font-YekanBakh text-zinc-700 font-thin tracking-tight">
             {item}
