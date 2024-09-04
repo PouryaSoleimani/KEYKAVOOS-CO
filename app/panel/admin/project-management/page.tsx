29e0c44fd00bb4543ae5e64a397f6448c5a8bcbc
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-09-04 21:57:35 +0330

    ADMIN PANEL

--- app/panel/admin/project-management/page.tsx
+++ app/panel/admin/project-management/page.tsx
@@ -6,7 +6,7 @@ function ProjectMangement() {
   return (
     <div className="grid grid-cols-1 gap-2">
       <div className="flex">
-        <Link href={`/panel/admin/project-management/order-management`} className="text-white bg-[#4866CF] p-3 rounded-[5px] hover:bg-blue-800 duration-300"  >
+        <Link href={`/panel/admin/project-management/order-management`} className="text-white bg-[#4866CF] py-3 px-6 rounded-[5px] hover:bg-blue-800 duration-300"  >
           مدیریت سفارشات
         </Link>
       </div>
