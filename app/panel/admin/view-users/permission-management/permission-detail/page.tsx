86b9b93c3776a92872e30ca04661178c120677d9
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-09-04 17:51:04 +0330

    ADMIN PANEL - PERMISSIONS

--- app/panel/admin/view-users/permission-management/permission-detail/page.tsx
+++ app/panel/admin/view-users/permission-management/permission-detail/page.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 import { getPermissionDetail } from "@/utils/utils";
+import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
@@ -8,23 +10,19 @@ function PermissionDetail() {
   const { token } = useSelector((state: any) => state.userData);
   const params = useSearchParams();
   const id = params.get("id");
-  const [permissionDetail, setPermissionDetail] = useState({
-    name_en: "",
-    name_fa: "",
-  });
+  const [permissionDetail, setPermissionDetail] = useState({ name_en: "", name_fa: "", });
 
-  useEffect(() => {
-    getPermissionDetail(token, id, setPermissionDetail);
-  }, []);
+  useEffect(() => { getPermissionDetail(token, id, setPermissionDetail); }, []);
 
   return (
-    <div className="bg-white shadow mx-auto rounded-2xl w-full p-[3%] text-center">
+    <div className="bg-white shadow mx-auto rounded-2xl w-full p-[3%] text-center tracking-tight font-semibold">
+      <Link rel="stylesheet" href="/panel/admin/view-users/permission-management" > ⬅ </Link>
       <div className="grid grid-cols-2">
         <div>نام به فارسی</div>
         <div>نام به انگلیسی</div>
       </div>
 
-      <div className="grid grid-cols-2 py-1 bg-[#EAEFF6] rounded-[4px] cursor-pointer">
+      <div className="grid grid-cols-2 py-3 bg-[#EAEFF6] rounded-[4px] my-3 cursor-pointer tracking-tight font-thin">
         <p>{permissionDetail.name_fa}</p>
         <p>{permissionDetail.name_en}</p>
       </div>
