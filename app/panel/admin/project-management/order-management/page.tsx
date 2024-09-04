2fd6d28e43cd1f52dbfdbbac913e730fc17d424f
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-09-04 21:49:06 +0330

    ADMIN PANEL - ORGANIZATIONS

--- app/panel/admin/project-management/order-management/page.tsx
+++ app/panel/admin/project-management/order-management/page.tsx
@@ -18,6 +18,7 @@ function OrderManagement() {
   const [orderStatus, setOrderStatus] = useState({ loading: false, error: "", });
 
   useEffect(() => { getOrders(token, setOrders, setOrderStatus); }, []);
+
   return (
     <>
       <div className="flex items-center justify-end py-2" >
