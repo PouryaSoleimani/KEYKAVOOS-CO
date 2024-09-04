d2967debbefbc3ac8c3e15f08275e65333ad9d52
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-09-04 17:13:18 +0330

    USER PANEL

--- app/panel/user/settings/legal.tsx
+++ app/panel/user/settings/legal.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Legal() {
 
-  return <div className="bg-[#EAEFF6] text-center p-4 text-red-700 font-semibold rounded-[4px]">
-    <p>امکان ویرایش اطلاعات وجود ندارد.</p>
+  return <div className="bg-red-100 text-center px-4 py-5 text-red-700 font-semibold rounded-[4px]">
+    <p className="bg-red-100 text-red-700 tracking-tight rounded-lg">امکان ویرایش اطلاعات وجود ندارد.</p>
   </div>;
 }
 
