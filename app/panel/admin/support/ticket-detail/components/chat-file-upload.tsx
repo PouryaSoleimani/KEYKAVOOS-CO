0865cc9076f5aacce5f2c9e47d9f9d6d0eba9ac4
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-09-05 21:50:54 +0330

    ADMIN PANEL

--- app/panel/admin/support/ticket-detail/components/chat-file-upload.tsx
+++ app/panel/admin/support/ticket-detail/components/chat-file-upload.tsx
@@ -1,15 +1,13 @@
 import React, { ChangeEvent } from "react";
-type FileUploadProps = {
-  handleChange: any;
-  File: any;
-};
+type FileUploadProps = { handleChange: any; File: any; };
+
 function ChatFileUpload({ handleChange, File }: FileUploadProps) {
+
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
-    if (file) {
-      handleChange(file);
-    }
+    if (file) { handleChange(file); }
   };
+
   return (
     <div>
       <div className="text-[#4866CE] bg-[#EAEFF6] p-2 rounded-[4px] duration-200 hover:bg-blue-800 hover:text-white">
