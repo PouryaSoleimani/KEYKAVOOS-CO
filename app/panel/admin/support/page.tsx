8fd5929954f326d1c385ece1104e49ae963d6e48
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-09-05 21:27:28 +0330

    ADMIN PANEL

--- app/panel/admin/support/page.tsx
+++ app/panel/admin/support/page.tsx
@@ -5,11 +5,7 @@ import add from "../../../../public/Panel/addticket.svg";
 import Link from "next/link";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
-import {
-  fetchUserProfile,
-  getIdFromLocal,
-  getTokenFromLocal,
-} from "@/redux/features/user/userSlice";
+import { fetchUserProfile, getIdFromLocal, getTokenFromLocal, } from "@/redux/features/user/userSlice";
 import checkmark from "../../../../public/Panel/checkmark.svg";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import vieweye from "../../../../public/ViewUsers/vieweye.svg";
@@ -21,16 +17,11 @@ const moment = require("moment-jalaali");
 const Support = () => {
   const { token } = useSelector((state: any) => state.userData);
   const [allTickets, setAllTickets] = useState([]);
-  const [allTicketsStatus, setAllTicketsStatus] = useState({
-    error: "",
-    loading: false,
-  });
+  const [allTicketsStatus, setAllTicketsStatus] = useState({ error: "", loading: false, });
   const [isClosed, setIsClosed] = useState(false);
   const [showModal, setShowModal] = useState(false);
 
-  useEffect(() => {
-    getAllTickets(token, setAllTickets, setAllTicketsStatus);
-  }, []);
+  useEffect(() => { getAllTickets(token, setAllTickets, setAllTicketsStatus); }, []);
 
   return (
     <div className="flex flex-col gap-3">
@@ -43,14 +34,11 @@ const Support = () => {
           closeTicketId={closeTicketId}
         />
       )} */}
-      <Link
-        href="/panel/admin/support/add-new-placard"
-        className="flex flex-row gap-2 bg-[#4866CE] text-white p-2 rounded-[4px] w-[120px]"
-      >
+      <Link href="/panel/admin/support/add-new-placard" className="flex flex-row gap-2 bg-[#4866CE] text-white p-3 rounded-[4px] w-[125px] hover:bg-blue-800 duration-300"  >
         <span>اعلان جدید</span>
         <Image src={add} alt="add" />
       </Link>
-      <div className="bg-white shadow mx-auto rounded-2xl py-[3%] px-[3%] w-full">
+      <div className="bg-white shadow mx-auto rounded-lg py-[3%] px-[3%] w-full">
         <div className="flex flex-col gap-5">
           <div className="grid grid-cols-5 text-center">
             <p>شماره</p>
@@ -67,10 +55,7 @@ const Support = () => {
             <NotFound text={`${allTicketsStatus.error}`} />
           ) : allTickets.length !== 0 ? (
             allTickets?.map((item: any, index) => (
-              <div
-                key={item.id}
-                className="grid grid-cols-5 text-center py-1 bg-[#EAEFF6] rounded-[4px]"
-              >
+              <div key={item.id} className="grid grid-cols-5 text-center py-1 bg-[#EAEFF6] rounded-[4px]"  >
                 <p>{index + 1}</p>
                 <p>{item.title}</p>
                 <div>
@@ -91,9 +76,9 @@ const Support = () => {
                 <p>
                   {item.updated_at
                     ? moment(
-                        item.updated_at,
-                        "YYYY-MM-DDTHH:mm:ss.SSSZ"
-                      ).format("jYYYY/jM/jD")
+                      item.updated_at,
+                      "YYYY-MM-DDTHH:mm:ss.SSSZ"
+                    ).format("jYYYY/jM/jD")
                     : "-"}
                 </p>
                 <div>
