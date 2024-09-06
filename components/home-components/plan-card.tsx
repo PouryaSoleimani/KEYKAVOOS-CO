adec9e4bc510a35062b3300db76a6463992239d5
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-09-06 12:59:44 +0330

    ADMIN PANEL

--- components/home-components/plan-card.tsx
+++ components/home-components/plan-card.tsx
@@ -4,6 +4,7 @@ import options from "../../public/Plans/options.svg";
 import Link from "next/link";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import styles from "./plan-card.module.css"
 
 type PlanCardProps = { data: CardInfo; };
 type CardInfo = { id: number; type: string; title: string; planInfo: string[]; price: string; imgSrc: string; route: string; };
@@ -15,35 +16,41 @@ function PlanCard({ data }: PlanCardProps) {
 
   return (
     <>
-      <div className="bg-zinc-300/30 backdrop-blur-[7px] w-[250px] md:max-lg:w-[260px] rounded-xl p-[3%] relative shadow hover:scale-105 duration-300 cursor-pointer">
-        <div className="bg-[#4866CF] w-[99%] mx-auto text-center text-[18px] font-semibold text-white rounded-t-xl tracking-tighter absolute z-10 -translate-x-1/2 left-1/2 -top-[0.04rem] p-2">
+      <div className="relative bg-zinc-300/30 shadow backdrop-blur-[7px] p-[3%] rounded-xl w-[250px] md:max-lg:w-[260px] cursor-pointer">
+        <div className="-top-[0.04rem] left-1/2 z-10 absolute bg-[#4866CF] mx-auto p-2 rounded-t-xl w-[99%] font-semibold text-[18px] text-center text-white tracking-tighter -translate-x-1/2">
           {data.type}
         </div>
-        <div className="border border-[#4866CF] rounded-xl">
-          <div className="p-[3%] mt-[18%] flex flex-col gap-5">
-            <p className="text-center text-[#68707A]  tracking-tighter" data-aos="zoom-in" data-aos-duration="1500" >{data.title}</p>
-            <div className="flex flex-col gap-3 justify-center items-center" data-aos="zoom-in" data-aos-duration="1500">
+        <div className="border-[#4866CF] border rounded-xl">
+          <div className="flex flex-col gap-5 mt-[18%] p-[3%]">
+            <p className="text-[#68707A] text-center tracking-tighter" data-aos="zoom-in" data-aos-duration="1500" >{data.title}</p>
+            <div className="flex flex-col justify-center items-center gap-3" data-aos="zoom-in" data-aos-duration="1500">
               <Image src={data.imgSrc} alt="news" />
               <div>
                 {data.planInfo.map((item, index) => (
                   <div key={index} className="flex flex-row items-center gap-1">
-                    <span className="text-[13px] text-[#68707A] leading-8">{item}</span>
+                    <span className="text-[#68707A] text-[13px] leading-8">{item}</span>
                     <Image src={options} alt="options" className="w-6 h-6" />
                   </div>
                 ))}
               </div>
-              <div className="border border-[#4866CF] rounded-xl overflow-hidden p-[3%] w-full">
-                <div className="text-[#4866CF] flex items-center flex-row-reverse justify-between">
+              <div className="border-[#4866CF] p-[3%] border rounded-xl w-full overflow-hidden">
+                <div className="flex flex-row-reverse justify-between items-center text-[#4866CF]">
                   <span className="text-[14px]">:شروع قیمت از</span>
-                  <span className="text-[48px] font-faNum font-bold">{data.price}</span>
-                  <p className="text-[12px] flex flex-col text-center ml-4 ">
+                  <span className="font-bold font-faNum text-[48px]">{data.price}</span>
+                  <p className="flex flex-col ml-4 text-[12px] text-center">
                     <span>میلیون</span>
                     <span>تومان</span>
                   </p>
                 </div>
                 <Link href={`${data.route}`}>
-                  <button className="text-white bg-[#4866CF] flex justify-center w-full rounded-lg py-[3%] hover:bg-blue-800 duration-300 ">
+                  {/* <button className="flex justify-center bg-[#4866CF] hover:bg-blue-800 py-[3%] rounded-lg w-full text-white duration-300">
                     توضیحات بیشتر
+                  </button> */}
+                  <button className={styles.plansbutton}>
+                    توضیحات بیشتر
+                    <svg fill="currentColor" viewBox="0 0 24 24" className={styles.icon}>
+                      <path clip-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z" fill-rule="evenodd"></path>
+                    </svg>
                   </button>
                 </Link>
               </div>
--- components/home-components/plans.tsx
+++ components/home-components/plans.tsx
@@ -3,14 +3,14 @@ import PlanCard from "./plan-card";
 import SectionHeader from "./section-header";
 import { PlanData } from "@/lib/data";
 import 'animate.css';
-
+import styles from './plans.module.css'
 
 //^ COMPONENT
 function Plans() {
   return (
-    <div className="relative flex flex-col animate__animated animate__delay-3s animate__fadeInUp animate__slow" id="PLANS__CONTAINER" >
+    <div className={`relative flex flex-col animate__animated animate__delay-3s animate__fadeInUp animate__slow px-4 py-16 ${styles.plansContainer} `} id="PLANS__CONTAINER" >
       <SectionHeader mainTitle="جدول خدمات طراحی سایت" subTitle="کدام طرح مناسب کسب و کار شماست؟" width="28%" />
-      <div className="flex sm:flex-row-reverse flex-col sm:max-lg:flex-wrap sm:justify-between items-center gap-5 lg:gap-0">
+      <div className={`flex sm:flex-row-reverse flex-col sm:max-lg:flex-wrap sm:justify-between items-center gap-5 lg:gap-0 `}>
         {PlanData.map((item) => (
           <div
             key={item.id}
