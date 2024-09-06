1a21f48ebd92ebdc9a21ed03c96042551d8fc0de
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-09-07 01:38:44 +0330

    HOME PAGE UI

--- app/page.tsx
+++ app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
           {/* <Plans /> */}
           <OurServices />
           <div className="relative px-6 web-container">
-          <h2 className='mb-10 relative -top-16 text-center font-[1000] py-1 bg-gradient-to-tr from-[#4866CF] via-blue-500 to-slate-700 bg-clip-text text-transparent text-5xl tracking-tighter animate__animated animate__fadeInUp animate__slow animate__delay-2s'>انواع مختلف طراحی و اجرای سایت </h2>
+            <h2 className='mb-10 relative -top-16 text-center font-[1000] py-1 bg-gradient-to-tr from-[#4866CF] via-blue-500 to-slate-700 bg-clip-text text-transparent text-4xl tracking-tighter animate__animated animate__fadeInUp animate__slow animate__delay-2s'> طراحی و اجرای سایت </h2>
             <WordPress />
             <WordPress />
             <WordPress />
--- components/home-components/OurServices.tsx
+++ components/home-components/OurServices.tsx
@@ -5,7 +5,7 @@ import 'animate.css';
 const OurServices = () => {
     return (
         <>
-            <h2 className='mt-32 mb-10 text-center font-[1000] py-1 bg-gradient-to-tr from-[#4866CF] via-blue-500 to-slate-700 bg-clip-text text-transparent text-5xl tracking-tighter animate__animated animate__fadeInUp animate__slow animate__delay-2s'>انواع خدمات ما </h2>
+            <h2 className='mt-32 mb-10 text-center font-[1000] py-1 bg-gradient-to-tr from-[#4866CF] via-blue-500 to-slate-700 bg-clip-text text-transparent text-4xl tracking-tighter animate__animated animate__fadeInUp animate__slow animate__delay-2s'> خدمات ما </h2>
             <div className='grid grid-cols-2 place-items-center lg:flex justify-around items-start py-10 gap-4 bg-[#EAEFF6] w-full lg:h-[500px]' dir='rtl' data-aos="fade-up" data-aos-duration="1500">
                 <div className="servicecard hover:scale-110 duration-500 cursor-pointer">
                     <div className="servicecard-overlay"></div>
