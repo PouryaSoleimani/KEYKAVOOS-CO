539445cd8ba8371a6ad06c72f70095c445e5931b
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-09-06 13:45:38 +0330

    ADMIN PANEL

--- components/home-components/reason-card.tsx
+++ components/home-components/reason-card.tsx
@@ -2,41 +2,27 @@
 import Image from "next/image";
 import React, { useState } from "react";
 
-type ReasonCardProps = {
-  data: ReasonInfo;
-};
-type ReasonInfo = {
-  id: number;
-  reasonTitle: string;
-  reasonText: string;
-  imgSrc: string;
-  hoveredImgSrc: string;
-};
+type ReasonCardProps = { data: ReasonInfo; };
+type ReasonInfo = { id: number; reasonTitle: string; reasonText: string; imgSrc: string; hoveredImgSrc: string; };
 
 function ReasonCard({ data }: ReasonCardProps) {
   const [isHovered, setIsHovered] = useState(false);
 
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-  };
+  const handleMouseEnter = () => { setIsHovered(true); };
 
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
+  const handleMouseLeave = () => { setIsHovered(false); };
+  
   //^ RETURN
   return (
-    <div className="bg-white lg:w-[220px] lg:h-[220px] rounded-lg flex flex-col items-center text-center gap-2 p-[5%] shadow cursor-pointer duration-500 hover:scale-105"
+    <div className="flex flex-col items-center gap-2 bg-transparent shadow-md shadow-zinc-700 backdrop-blur-[1px] px-2 py-6 rounded-lg lg:w-[295px] lg:h-[220px] text-center duration-500 cursor-pointer hover:scale-105"
       style={{ backgroundColor: isHovered ? "#4866CF" : "white", color: isHovered ? "white" : "black", }}
       onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}
     >
-      <p className="text-[20px] font-semibold tracking-tighter" style={{ color: isHovered ? "white" : "#4866CF" }}  >
+      <p className="font-semibold text-[22px] tracking-tighter" style={{ color: isHovered ? "white" : "#4866CF" }}  >
         {data.reasonTitle}
       </p>
       <Image src={isHovered ? data.hoveredImgSrc : data.imgSrc} alt={data.reasonTitle} />
-      <p
-        className="text-[16px] leading-6 tracking-tight mt-6 text-center"
-        style={{ color: isHovered ? "white" : "black" }}
-      >
+      <p className="mt-6 text-[16px] text-center leading-6 tracking-tight" style={{ color: isHovered ? "white" : "black" }}  >
         {data.reasonText}
       </p>
     </div>
