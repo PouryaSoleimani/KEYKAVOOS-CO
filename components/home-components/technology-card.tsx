061895ebd4ad03c1a86e3a7f898214725bdc499b
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-09-06 13:55:02 +0330

    ADMIN PANEL

--- components/home-components/technology-card.tsx
+++ components/home-components/technology-card.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from 'aos';
+import 'aos/dist/aos.css'
 
 type TechnologyCardProps = {
   technologyInfo: { id: number; title: string; imgUrl: string; technologies: { id: number; techImgUrl: string[]; tech: string[]; }[]; };
@@ -7,8 +9,12 @@ type TechnologyCardProps = {
 
 //^ COMPONENT
 function TechnologyCard({ technologyInfo }: TechnologyCardProps) {
+
+  useEffect(() => { AOS.init(); }, [])
+
+
   return (
-    <div className="flex flex-col items-center gap-3 bg-transparent shadow-md shadow-zinc-700 hover:shadow-xl backdrop-blur-[2px] mx-auto mb-6 rounded-[15px] w-[260px] lg:w-full h-[400px] md:max-lg:h-[300px] duration-200 cursor-pointer hover:scale-105">
+    <div className="flex flex-col items-center gap-3 bg-transparent shadow-md shadow-zinc-700 hover:shadow-xl backdrop-blur-[2px] mx-auto mb-6 rounded-[15px] w-[260px] lg:w-full h-[400px] md:max-lg:h-[300px] duration-200 cursor-pointer hover:scale-105" >
       <div className="bg-[#4866CF] mb-3 p-2 rounded-t-lg w-full font-bold text-[20px] text-center text-white tracking-tighter">
         {technologyInfo.title}
       </div>
@@ -16,10 +22,10 @@ function TechnologyCard({ technologyInfo }: TechnologyCardProps) {
       <div
         style={{ borderTop: "2.5px solid", borderImage: "linear-gradient(270deg,  #EAEFF6 0%, #4866CF 50%,#EAEFF6 100%) 1", width: "80%", height: "2.5px", }}
       ></div>
-      <ul className="flex flex-col items-center gap-5 py-4 w-full">
+      <ul className="flex flex-col items-center gap-5 py-4 w-full" data-aos="zoom-in" data-aos-duration="1500">
         {technologyInfo.technologies.map(
           (tech: { id: number; techImgUrl: string[]; tech: string[] }, index) => (
-            <li key={tech.id} className="flex flex-row items-center gap-3 px-6 py-2 w-full text-center">
+            <li key={tech.id} className="flex flex-row items-center gap-3 px-6 py-2 w-full text-center" >
               {tech.techImgUrl.map((url, index) => (
                 <Image key={index} src={url} alt={""} width={34} height={34} />
               ))}
