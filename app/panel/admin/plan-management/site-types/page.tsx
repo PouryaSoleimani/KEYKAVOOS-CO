929de9942115d49b778b3fb72d4d7deaa8f5162a
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-09-06 10:31:55 +0330

    ADMIN PANEL

--- app/panel/admin/plan-management/site-types/page.tsx
+++ app/panel/admin/plan-management/site-types/page.tsx
@@ -24,10 +24,7 @@ function SiteTypes() {
   return (
     <div className="grid grid-cols-1 gap-5">
       <div className="flex">
-        <NewInfoOnEachPageBtn
-          btnText="ایجاد نوع طراحی"
-          src="/panel/admin/plan-management/site-types/create-site-type"
-        />
+        <NewInfoOnEachPageBtn btnText="ایجاد نوع طراحی" src="/panel/admin/plan-management/site-types/create-site-type" />
       </div>
       <div className="bg-white shadow mx-auto rounded-2xl w-full p-[3%] text-center space-y-3">
         <div className="grid grid-cols-4">
@@ -40,16 +37,8 @@ function SiteTypes() {
         {siteTypes.map((item: any, index) => (
           <div className={`${siteTypeIsDeleted && item.deleted_at ? "bg-red-300" : "bg-[#EAEFF6]"} grid grid-cols-4 gap-x-5 text-center py-1 rounded-[4px] cursor-pointer`} key={index}>
             <p>{index + 1}</p>
-            <input
-              value={item.title ? item.title : "-"}
-              readOnly={true}
-              className={`${editField.showEditField ? "bg-white" : "bg-[#EAEFF6] caret-transparent cursor-default text-center"} outline-none`}
-            />
-            <input
-              value={item.description ? item.description : "-"}
-              readOnly={true}
-              className={`${editField.showEditField ? "bg-white" : "bg-[#EAEFF6] caret-transparent cursor-default text-center"} outline-none`}
-            />
+            <input value={item.title ? item.title : "-"} readOnly={true} className={`${editField.showEditField ? "bg-white" : "bg-[#EAEFF6] caret-transparent cursor-default text-center"} outline-none`} />
+            <input value={item.description ? item.description : "-"} readOnly={true} className={`${editField.showEditField ? "bg-white" : "bg-[#EAEFF6] caret-transparent cursor-default text-center"} outline-none`} />
             <div className="flex flex-row items-center justify-center gap-3">
               <Link href={`/panel/admin/plan-management/site-types/site-type-detail?id=${item.id}`} className="flex justify-center"  >
                 <Image src={vieweye} alt="مشاهده" width={20} height={20} />
