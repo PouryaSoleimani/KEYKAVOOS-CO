d947ece80ba34044d73c576dcdc18860f3dcc809
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-09-06 05:00:23 +0330

    ADMIN PANEL

--- app/panel/admin/notifications-management/add-new-placard/new-placard.tsx
+++ app/panel/admin/notifications-management/add-new-placard/new-placard.tsx
@@ -6,87 +6,80 @@ import { createNotification } from "@/utils/utils";
 
 type NewPlacardProps = { setSteps: React.Dispatch<React.SetStateAction<number>>; };
 
+
+
 function NewPlacard({ setSteps }: NewPlacardProps) {
   // STATES
   const { token } = useSelector((state: any) => state.userData);
   const [users, setUsers] = useState([]);
-  const [departments, setDepartments] = useState([]);
+  const [departments, setDepartments] = useState([{ departments: { name_fa: "" } }]);
   const [brands, setBrands] = useState([]);
-  const [annonceInfo, setAnnounceInfo] = useState({ description: "", dept_id: "", user_id: "", brand_id: "", });
+  const [annonceInfo, setAnnounceInfo] = useState({ description: "", dept_id: "", user_id: "", brand_id: "", firstuser: { name: "" } });
+
 
   // FUNCTIONS
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const localUsers = JSON.parse(
-        window.sessionStorage.getItem("users") as string
-      );
-      setUsers(localUsers);
+  // useEffect(() => {
+  //   if (typeof window !== "undefined") {
+  //     const localUsers = JSON.parse(window.sessionStorage?.getItem("users") as string);
+  //     setUsers(localUsers);
 
-      const localDepartments = JSON.parse(
-        window.sessionStorage.getItem("departments") as string
-      );
-      setDepartments(localDepartments);
+  //     const localDepartments = JSON.parse(window.sessionStorage?.getItem("departments") as string);
+  //     setDepartments(localDepartments);
 
-      const localBrands = JSON.parse(
-        window.sessionStorage.getItem("brands") as string
-      );
-      setBrands(localBrands);
-    }
-  }, []);
+  //     const localBrands = JSON.parse(window.sessionStorage?.getItem("brands") as string);
+  //     setBrands(localBrands);
+  //   }
+  // }, []);
 
-  useEffect(() => {
-    if (users.length > 0 && !annonceInfo.user_id) {
-      const firstUser = users[0]; // Get the first user
-      setAnnounceInfo((prev) => ({ ...prev, user_id: firstUser?.name })); // Set the user_id to the first user's name
-    }
-  }, [users]);
+  // useEffect(() => {
+  //   if (users.length > 0 && !annonceInfo.user_id) {
+  //     const firstUser = users[0]; // Get the first user
+  //     setAnnounceInfo((prev) => ({ ...prev, user_id: firstUser.name })); // Set the user_id to the first user's name
+  //   }
+  // }, [users]);
 
   // Automatically select the first department if available
-  useEffect(() => {
-    if (departments.length > 0 && !annonceInfo.dept_id) {
-      const firstDepartment = departments[0].? departments.?name_fa; // Get the first department name
-      setAnnounceInfo((prev) => ({ ...prev, dept_id: firstDepartment })); // Set the dept_id to the first department name
-    }
-  }, [departments]);
+  // useEffect(() => {
+  //   if (departments?.length > 0 && !annonceInfo.dept_id) {
+  //     const firstDepartment = departments[0].departments.name_fa; // Get the first department name
+  //     setAnnounceInfo((prev) => ({ ...prev, dept_id: firstDepartment })); // Set the dept_id to the first department name
+  //   } else { null }
+  // }, [departments]);
 
-  useEffect(() => {
-    if (brands.length > 0 && !annonceInfo.brand_id) {
-      const firstBrand = brands[0].?brand.title;
-      setAnnounceInfo((prev) => ({ ...prev, dept_id: firstBrand }));
-    }
-  }, [departments]);
+  // useEffect(() => {
+  //   if (brands.length > 0 && !annonceInfo.brand_id) {
+  //     const firstBrand = brands[0].? brand.title;
+  //     setAnnounceInfo((prev) => ({ ...prev, dept_id: firstBrand }));
+  //   }
+  // }, [departments]);
 
-  const departmentInfo = departments.map(
-    (item: { department: { id: number; name_fa: string } }) =>
-      item.department.id + "-" + item.department.name_fa
-  );
+  // const departmentInfo = departments?.map(
+  //   (item: { department: { id: number; name_fa: string } }) => item.department.id + "-" + item.department.name_fa
+  // );
 
-  const usersInfo = users.map(
-    (item: { name: string; surname: string }) => item.name + " " + item.surname
-  );
+  // const usersInfo = users?.map((item: { name: string; surname: string }) => item.name + " " + item.surname);
 
-  const brandInfo = brands.map((item: { brand: { title: string } }) => item.brand?.title);
+  // const brandInfo = brands?.map((item: { brand: { title: string } }) => item.brand?.title);
 
-  const depId = departments
-    .filter((item: { department: { name_fa: string } }) => annonceInfo?.dept_id?.includes(item.department.name_fa))
-    .map((item: { department: { id: number } }) => item.department.id)[0];
+  // const depId = departments
+  //   .filter((item: { department: { name_fa: string } }) => annonceInfo?.dept_id?.includes(item.department.name_fa))
+  //   .map((item: { department: { id: number } }) => item.department.id)[0];
 
-  const userId = users
-    .filter((item: { name: string }) => annonceInfo?.user_id?.includes(item.name))
-    .map((item: { id: number }) => item.id)[0];
+  // const userId = users?.filter((item: { name: string }) => annonceInfo?.user_id?.includes(item.name))?.map((item: { id: number }) => item.id)[0];
 
-  const brandId = brands
-    .filter((item: { brand: { title: string } }) => item.brand?.title.includes(annonceInfo.brand_id))
-    .map((item: { brand: { id: number } }) => item.brand?.id)?.[0];
+  // const brandId = brands?.filter((item: { brand: { title: string } }) => item.brand?.title.includes(annonceInfo.brand_id))?.map((item: { brand: { id: number } }) => item.brand?.id)?.[0];
 
-  const handleSubmission = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    await createNotification(token, Number(depId), Number(brandId), Number(userId), annonceInfo.description);
-    setAnnounceInfo((last) => ({ ...last, description: "" }));
-  };
+  // const handleSubmission = async (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   await createNotification(token, Number(depId), Number(brandId), Number(userId), annonceInfo.description);
+  //   setAnnounceInfo((last) => ({ ...last, description: "" }));
+  // };
 
+
+  // RETURN
   return (
-    <form onSubmit={(e) => handleSubmission(e)} className="grid grid-cols-1 gap-3" >
+    // <form onSubmit={(e) => handleSubmission(e)} className="grid grid-cols-1 gap-3" >
+    <form className="grid grid-cols-1 gap-3" >
       <div className="grid grid-cols-2 gap-8">
         <CostumSelect
           changeHandler={(e: React.ChangeEvent<HTMLInputElement>) => setAnnounceInfo((last) => ({ ...last, dept_id: e.target.value }))}
