718ff785246216dc4bd8c5596c89bf3d55d8047c
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-09-09 19:54:19 +0330

    REBRANDING

--- app/page.tsx
+++ app/page.tsx
@@ -40,14 +40,11 @@ export default function Home() {
   } else {
     //^ RETURN
     return (
-      // <div className="bg-[#EAEFF6]">
       <div className="bg-zinc-800">
         <Script src="/navachat.js" async />
         <Nav />
         <main className="flex flex-col mx-auto my-[3%] w-[80%]">
           <Intro />
-          {/* <IntroStatistics /> */}
-          {/* <Plans /> */}
           <OurServices />
           <div className="relative px-6 web-container">
             <h2 className='mb-10 relative -top-16 text-center font-[1000] py-1 bg-gradient-to-tr from-[#4866CF] via-blue-500 to-slate-700 bg-clip-text text-transparent text-4xl tracking-tighter animate__animated animate__fadeInUp animate__slow animate__delay-2s'> طراحی و اجرای سایت </h2>
