3fa5d4774aaf22fa6c3c27056cb54314e60511ba
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-10-03 22:20:15 +0330

    UI

--- app/blog/components/BlogCard.tsx
+++ app/blog/components/BlogCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaCircle } from 'react-icons/fa6'
 interface props { imageSrc: string, title: string, content: string }
 
 
@@ -9,12 +10,16 @@ const BlogCard: React.FC<props> = ({ imageSrc, title, content }) => {
         <div>
             <div className="w-72 card backdrop-blur-[2px] bg-zinc-800/10 shadow-lg shadow-zinc-300 hover:scale-110 duration-500 cursor-pointer gap-y-4 " dir='rtl'>
                 <figure>
-                    <img src={imageSrc} alt="car!" />
+                    <img src={imageSrc} alt="Pic" />
                 </figure>
-                <div className="card-body gap-y-5">
+                <div className="card-body gap-y-4">
+                    <p className='text-xs text-blue-600 flex flex-row justify-start items-center gap-x-1 w-fit'>
+                        <FaCircle className='p-[2px]' />
+                        دسته بندی مطلب
+                    </p>
                     <h2 className="card-title">{title}</h2>
-                    <p>{content}</p>
-                    <div className="justify-end card-actions">
+                    <p className='text-zinc-600'>{content}</p>
+                    <div className="justify-end card-actions ">
                         <button className="btn bg-[#4866CF] font-extralight hover:bg-blue-800 text-white text-lg text-start w-full rounded-xl duration-300" dir='rtl'>بیشتر ...</button>
                     </div>
                 </div>
