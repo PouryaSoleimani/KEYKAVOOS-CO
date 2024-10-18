9a2c3f0056b681b580f1581d718c393a5acdd6f7
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-10-18 12:45:11 +0330

    UI REFACTOR

--- components/home-components/technology.tsx
+++ components/home-components/technology.tsx
@@ -6,7 +6,7 @@ import styles from './plans.module.css'
 
 function Technology() {
   return (
-    <div className={`flex flex-col mt-2 px-6`} data-aos="fade-up" data-aos-duration="1500">
+    <div className={`flex flex-col mt-2 lg:px-6`} data-aos="fade-up" data-aos-duration="1500">
       <SectionHeader mainTitle="تکنولوژی های طراحی و راه اندازی سایت در کیکاووس" subTitle="کدام طرح مناسب کسب و کار شماست؟" width="20%" />
       <div className="gap-5 md:max-lg:gap-16 lg:gap-x-16 grid place-items-center grid-cols-1 md:max-lg:grid-cols-2 lg:grid-cols-3 mt-16 lg:mt-10 lg:px-8">
         {TechnologyData.map((item) => (
