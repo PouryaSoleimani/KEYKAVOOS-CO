703369858603eb52db21b317b8a6d79926debce7
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-10-18 03:40:31 +0330

    AXIOS PROBLEM RESOLVED

--- app/panel/admin/org_management/org-detail/page.tsx
+++ app/panel/admin/org_management/org-detail/page.tsx
@@ -9,29 +9,21 @@ import NotFound from "../../components/NotFound";
 import { number } from "yup";
 import { IoArrowBack } from "react-icons/io5";
 import Link from "next/link";
-import app from "@/services/service";
-export type BrandDetailType = { title: string; description: string; };
-type SingleOrgDetailType = { id: string, name: string, descriprion: string, phone: string, shenase_melli: string, user: string, address: string, logo: string }
 
-// ^ COMPONENT
+export type BrandDetailType = {
+  title: string;
+  description: string;
+};
 function OrgDetail() {
   const { token } = useSelector((state: any) => state.userData);
   const params = useSearchParams();
   const id = params.get("id");
   const [orgDetailStatus, setOrgDetailStatus] = useState({ loading: false, error: "", });
-  const [orgDetail, setOrgDetail] = useState<SingleOrgDetailType>({ id: "", name: "", descriprion: "", phone: "", shenase_melli: "", user: "", address: "", logo: "" });
-  const orgDetailArray = Array(orgDetail)
+  const [orgDetail, setOrgDetail] = useState({ id: "", name: "", descriprion: "", phone: "", shenase_melli: "", user: "", address: "", logo: "" });
 
-  //FUNCTIONS
-  // useEffect(() => { getOrganizationDetail(token, id, setOrgDetail, setOrgDetailStatus); }, []);
-
-  const getSingleOrgDetail = (id: number | string) => {
-    app.get(`/organization/show/${id}`)
-      .then(response => { console.log("response =>", response); setOrgDetail(response.data); console.log("SINGLE ORG DETAIL =>", orgDetail) })
-      .catch((error: any) => console.log("%c ERROR ===>", error))
-  }
-  useEffect(() => { getSingleOrgDetail(id as string) }, [])
-
+  useEffect(() => {
+    getOrganizationDetail(token, id, setOrgDetail, setOrgDetailStatus);
+  }, []);
 
   return (
     <>
@@ -51,22 +43,22 @@ function OrgDetail() {
           <div>شناسه ملی</div>
           <div>لوگو</div>
         </div>
-        {orgDetailStatus?.loading ? (
+        {orgDetailStatus.loading ? (
           <SkeletonTheme>
             <Skeleton count={1} className="p-2" baseColor="#EAEFF6" />
           </SkeletonTheme>
-        ) : orgDetailStatus?.error ? (
-          <NotFound text={`${orgDetailStatus?.error}`} />
+        ) : orgDetailStatus.error ? (
+          <NotFound text={`${orgDetailStatus.error}`} />
         ) : (
-          <div key={orgDetail?.id} className="grid grid-cols-8 py-3 bg-[#EAEFF6] rounded-[4px] place-orgDetails-center">
-            <p>{orgDetail?.id}</p>
-            <p>{orgDetail?.name}</p>
-            <p>{orgDetail?.descriprion ? orgDetail?.descriprion : "-"}</p>
-            <p>{orgDetail?.user}</p>
-            <p>{orgDetail?.phone}</p>
-            <p>{orgDetail?.address}</p>
-            <p>{orgDetail?.shenase_melli ? orgDetail?.shenase_melli : "-"}</p>
-            <p>{orgDetail?.logo ? orgDetail?.logo : "---"}</p>
+          <div className="grid grid-cols-8 py-3 bg-[#EAEFF6] rounded-[4px] place-items-center">
+            <p>{orgDetail.id}</p>
+            <p>{orgDetail.name}</p>
+            <p>{orgDetail.descriprion ? orgDetail.descriprion : "-"}</p>
+            <p>{orgDetail.user}</p>
+            <p>{orgDetail.phone}</p>
+            <p>{orgDetail.address}</p>
+            <p>{orgDetail.shenase_melli ? orgDetail.shenase_melli : "-"}</p>
+            <p>{orgDetail.logo ? orgDetail.logo : "---"}</p>
           </div>
         )}
       </div>
