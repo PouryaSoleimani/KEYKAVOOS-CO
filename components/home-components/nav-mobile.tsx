ad861cafd56ba1b081d94f252902e2d5ac53613b
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-10-18 13:20:56 +0330

    UI REFACTOR

--- components/home-components/nav-mobile.tsx
+++ components/home-components/nav-mobile.tsx
@@ -10,34 +10,10 @@ const NavMobile = ({ localToken, userProfile, active, setActive, setShowFour, se
 
   return (
     <div className="lg:hidden">
-      <Image
-        src={active ? "/cross.jpg" : "/bar.svg"}
-        width={active ? 25 : 40}
-        height={active ? 25 : 40}
-        alt="info"
-        onClick={() => (setActive(!active), setShowOne(false), setShowTwo(false), setShowThree(false), setShowFour(false))}
-        className={`z-20 relative ${active ? "-top-1" : "top-0"}`}
-      />
+      <Image src={active ? "/cross.jpg" : "/bar.svg"} width={active ? 25 : 40} height={active ? 25 : 40} alt="info" onClick={() => (setActive(!active), setShowOne(false), setShowTwo(false), setShowThree(false), setShowFour(false))} className={`z-20 relative ${active ? "-top-1" : "top-0"}`} />
       <ul className={`w-full flex flex-col justify-center items-end list-none border-b-[10px] border-b-indigo-500 ${active ? "absolute top-0 left-0 w-full p-4 bg-white z-10 rounded-xl" : "hidden"}`}>
         <li className={style.list}>
-          {showOne && (
-            <ul className="list-none left-64 bg-white rounded-2xl border-b-8 border-b-[#4866CF] text-right flex flex-col z-10 absolute top-12 gap-4 w-[30%] px-4 py-3">
-              <Link href="/weblog/back-end ">
-                <li className="text-sm font-semibold">بک اند</li>
-              </Link>
-              <Link href="/weblog/front-end">
-                <li className="text-sm font-semibold">فرانت اند</li>
-              </Link>
-              <Link href="/weblog/accounting">
-                <li className="text-sm font-semibold">حسابداری</li>
-              </Link>
-              <Link href="/weblog/digital-marketing">
-                <li className="text-xs rounded-b-lg font-semibold whitespace-nowrap px-1">
-                  دیجیتال مارکتینگ
-                </li>
-              </Link>
-            </ul>
-          )}
+          {showOne && (<Link href="/blog" className="list-none left-64 bg-white rounded-2xl border-b-8 border-b-[#4866CF] text-right flex flex-col z-10 absolute top-12 gap-4 w-[30%] px-4 py-3"></Link>)}
           <button className="bg-[#4866CF] p-2 rounded-lg text-white relative -left-7">
             <Link
               href={
@@ -63,15 +39,7 @@ const NavMobile = ({ localToken, userProfile, active, setActive, setShowFour, se
                 `${userProfile.FirstName} ${userProfile.LastName}`}
             </Link>
           </button>
-          <span>
-            <Image src="/arrow.png" width={22} height={22} alt="arrow" className={`${showOne ? "rotate-90" : "rotate-180"}`} onClick={() => (setShowOne(!showOne), setShowTwo(false), setShowThree(false), setShowFour(false))} />
-          </span>
-          <span
-            className="font-bold text-lg"
-            onClick={() => (setShowOne(!showOne), setShowTwo(false), setShowThree(false), setShowFour(false))}
-          >
-            وبلاگ
-          </span>
+          <Link href='/blog' className="font-bold text-lg" onClick={() => (setShowOne(!showOne), setShowTwo(false), setShowThree(false), setShowFour(false))}  > وبلاگ  </Link>
         </li>
         <li className={style.list}>
           {showTwo && (
