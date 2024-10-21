3abeaba9d42f05d09cdffe90d691e345cf585a7a
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-10-22 02:40:09 +0330

    DEBUGGING

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const nextConfig = {
   productionBrowserSourceMaps: true,
   images: { domains: ["keykavoos.liara.run", "localhost", "localhost:8000/storage", "localhost:3000"], },
   images: { remotePatterns: [{ protocol: "http", hostname: "**", }, { protocol: "http", port: "3000", hostname: "***" }], },
-  turbo: {},
 };
 const withBundleAnalyzer = require('@next/bundle-analyzer')({ enabled: process.env.ANALYZE === 'true', })
 
