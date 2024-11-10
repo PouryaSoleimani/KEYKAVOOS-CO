37617fd5941eb9a80cae65d61beae90feef2075c
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-11-11 01:04:32 +0330

    REFACTORg

--- components/home-components/OrderNew.tsx
+++ components/home-components/OrderNew.tsx
@@ -18,11 +18,14 @@ const OrderNew = () => {
 
     const { register, handleSubmit, formState: { errors }, reset } = useForm({ resolver: yupResolver(schema) });
 
-    const onSubmit: SubmitHandler<Inputs> = (data) => { console.log(data)  ; reset}
+    const onSubmit: SubmitHandler<Inputs> = (data) => {
+        console.log(data); reset()
+    }
+
     return (
-        <div className='TECH-CARD w-[88%] h-[28rem] bg-zinc-100/30 backdrop-blur-sm mx-auto my-6 rounded-xl  shadow-md shadow-zinc-400 border border-zinc-300'>
+        <div className='TECH-CARD w-[88%] h-[27rem] bg-zinc-100/30 backdrop-blur-sm mx-auto my-6 rounded-xl shadow-md shadow-zinc-400 border border-zinc-300'>
             <label className="text-[#4866CF] flex justify-center sm:text-[36px] text-[30px] my-4 tracking-tighter "> فرم ثبت درخواست مشاوره </label>
-            <form className='flex flex-col space-y-6 my-6' onSubmit={handleSubmit(onSubmit)}>
+            <form className='flex flex-col space-y-7 my-6' onSubmit={handleSubmit(onSubmit)}>
                 <div id="FORM___TOP" className='flex items-center justify-center gap-x-2 px-12' dir='rtl'>
 
                     <label className="input input-bordered flex items-center gap-2 border-2 border-slate-300 w-1/3">
@@ -49,7 +52,7 @@ const OrderNew = () => {
                     </label>
 
                 </div>
-                <button type="submit" className="text-white ml-12 bg-gradient-to-r from-blue-500 via-blue-600 to-blue-800 hover:bg-gradient-to-br duration-300 w-44 text-xl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg px-5 py-2.5 text-center me-2 mb-2"> ثبت درخواست </button>
+                <button type="submit" className="text-white -translate-y-4 ml-12 bg-gradient-to-r from-blue-500 via-blue-600 to-blue-800 hover:bg-gradient-to-br duration-300 w-44 text-xl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg px-5 py-2.5 text-center me-2 mb-2"> ثبت درخواست </button>
             </form>
         </div>
     )
