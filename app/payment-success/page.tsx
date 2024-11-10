6620ef3e68210d758ffc7d3a51afedd0213405fd
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-11-10 20:20:49 +0330

    RESPONSIVE

--- app/payment-success/page.tsx
+++ app/payment-success/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const PaymentSuccessPage = () => {
@@ -7,7 +8,7 @@ const PaymentSuccessPage = () => {
             <div className='bg-green-900 w-1/2 h-1/3 backdrop-blur-md border-emerald-800 border-8 rounded-2xl flex flex-col py-4 space-y-6 items-center justify-center'>
                 <Image src='/PAYMENTS/PAYMENT___SUCCESS.png' width={130} height={130} alt='payment-success'className='mx-auto' />
                 <h1 className='text-3xl font-extrabold text-center text-white my-2 tracking-tighter'>پرداخت شما با موفقیت انجام شد</h1>
-                <button className='bg-green-950 text-white px-4 py-1.5 rounded-md text-lg hover:bg-green-600 duration-300'>بازگشت به داشبورد</button>
+                <Link href="/panel/user/dashboard" className='bg-green-950 text-white px-4 py-1.5 rounded-md text-lg hover:bg-green-600 duration-300'>بازگشت به داشبورد</Link>
             </div>
         </div>
     )
