cbc7243547ad9a8ef0ac254777b79ec84551c6c2
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-11-10 20:37:04 +0330

    RESPONSIVE

--- components/home-components/news.tsx
+++ components/home-components/news.tsx
@@ -45,14 +45,14 @@ function News() {
       onSubmit={(e) => handleSubmission(e)}
       className="bg-gradient-to-tr from-zinc-200/50 backdrop-blur-[1px] via-blue-100/30 to-[#4866CF20] h-[400px] mt-[5%] flex flex-col justify-center items-center gap-3 px-[3%] sm:px-0 relative z-10 overflow-hidden"
     >
-      <p className="text-[#334052] sm:text-[36px] font-extrabold py-4 max-w-lg text-center text-[30px] tracking-tighter" >
+      <p className="text-[#334052] sm:text-[30px] font-bold py-4 max-w-lg text-center text-[30px] tracking-tighter" >
         با عضویت در خبرنامه شرکت بین المللی کیکاووس زمان از تخفیفات باخبر شوید
       </p>
       <div className="bg-[#F8FAFC] sm:w-[400px] flex flex-row rounded-2xl shadow-md shadow-zinc-400 p-0 ">
-        <button className="bg-zinc-600 text-white w-[130px] m-0 py-3 rounded-l-2xl hover:text-white hover:bg-[#4866CF] duration-300" type="submit"  >
+        <button className="bg-zinc-600 text-white w-[130px] m-0 py-3 rounded-l-xl hover:text-white hover:bg-[#4866CF] duration-300" type="submit"  >
           تایید ایمیل
         </button>
-        <input className="outline-none w-full rounded-r-2xl px-3" dir="rtl" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input className="outline-none w-full rounded-r-xl px-3" dir="rtl" value={email} onChange={(e) => setEmail(e.target.value)} />
       </div>
       <Image src="/Newsletter/bottomsvg-newsletterbg-icon.svg" alt="bg" className="absolute -left-16 -bottom-20 -z-10 max-lg:hidden opacity-50 w-80" width={25} height={25} />
       <Image src="/Newsletter/bottomsvg-newsletterbg-icon.svg" alt="bg" className="absolute -right-16 -top-20 rotate-180 opacity-50 max-lg:hidden w-80 " width={25} height={25} />
--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   typescript: { ignoreBuildErrors: true, },
   eslint: { ignoreDuringBuilds: true, },
   productionBrowserSourceMaps: true,
-  images: { domains: ["keykavoos.liara.run", "localhost" , "back.keykavoos.co/storage" , "localhost:3000",], },
+  images: { domains: ["localhost" , "back.keykavoos.co/storage" , "localhost:3000",], },
   images: { remotePatterns: [{ protocol: "https", hostname: "**", }, { protocol: "http", port: "3000", hostname: "***" }], },
 };
 const withBundleAnalyzer = require('@next/bundle-analyzer')({ enabled: process.env.ANALYZE === 'true', })
