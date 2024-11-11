0213858ed468d1254df1d359ce1d542bf48fb404
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-11-11 23:11:50 +0330

    RESPONSIVE

--- components/home-components/OurServices.tsx
+++ components/home-components/OurServices.tsx
@@ -9,27 +9,27 @@ const OurServices = () => {
         <>
             <h2 id='OUR__SERVICES__SECTION' className='mt-32 mb-10 text-center font-[1000] bg-gradient-to-tr from-blue-500 via-[#4866CF] to-blue-800 bg-clip-text text-transparent text-5xl tracking-tighter animate__animated animate__fadeInUp animate__slow animate__delay-2s'> خدمات ما </h2>
             <section className='grid grid-cols-1 sm:grid-cols-2 sm:grid-rows-3 sm:gap-16 sm:place-items-stretch lg:px-10 lg:flex flex-wrap  place-items-stretch px-10 justify-center items-start py-16 gap-4 w-full lg:h-[900px] lg:my-16 text-zinc-800' dir='rtl' data-aos="fade-up" data-aos-duration="1500">
-                <div className="servicecard hover:scale-110 duration-300 cursor-pointer z-10 backdrop-blur-[10px] bg-white shadow-md flex items-center justify-center shadow-zinc-300 rounded-2xl border border-zinc-300 text-center">
+                <div className="servicecard h-fit hover:scale-110 duration-300 cursor-pointer z-10 backdrop-blur-[10px] bg-white shadow-md flex items-center justify-center shadow-zinc-300 rounded-2xl border border-zinc-300 text-center">
                     <div className="servicecard-overlay backdrop-blur-sm z-0 bg-transparent rounded-2xl"></div>
                     <Link href='/our-services/web-application' className="servicecard-inner z-30 backdrop-blur-sm text-center text-4xl font-bold ">طراحی<br /><br /><br />وب اپلیکیشن</Link>
                     <Image src='/3D_ICONS/AT.png' alt='at_sign' width={170} height={170} className='wordPress3D absolute top-[-4rem] left-0 sm:left-[7.5rem] -z-10' />
                 </div>
-                <div className="servicecard hover:scale-110 duration-300 cursor-pointer z-10 backdrop-blur-[10px] bg-white shadow-md flex items-center justify-center shadow-zinc-300 rounded-2xl border border-zinc-300 text-center">
+                <div className="servicecard h-fit hover:scale-110 duration-300 cursor-pointer z-10 backdrop-blur-[10px] bg-white shadow-md flex items-center justify-center shadow-zinc-300 rounded-2xl border border-zinc-300 text-center">
                     <div className="servicecard-overlay backdrop-blur-sm z-0 bg-transparent  rounded-2xl"></div>
                     <Link href='/our-services/website' className="servicecard-inner z-30 backdrop-blur-sm">طراحی سایت</Link>
                     <Image src='/3D_ICONS/PC.png' alt='at_sign' width={170} height={170} className='wordPress3D absolute top-[-5rem] lg:top-[6rem] left-[-1rem] lg:left-[3rem] -z-10' />
                 </div>
-                <div className="servicecard hover:scale-110 duration-300 cursor-pointer z-10 backdrop-blur-[10px] bg-white shadow-md flex items-center justify-center shadow-zinc-300 rounded-2xl border border-zinc-300 text-center">
+                <div className="servicecard h-fit hover:scale-110 duration-300 cursor-pointer z-10 backdrop-blur-[10px] bg-white shadow-md flex items-center justify-center shadow-zinc-300 rounded-2xl border border-zinc-300 text-center">
                     <div className="servicecard-overlay backdrop-blur-sm z-0 bg-transparent  rounded-2xl"></div>
                     <Link href="/our-services/hardwares" className="servicecard-inner z-30 backdrop-blur-sm"> تامین <br /> قطعات  <br /> کامپیوتری</Link>
                     <Image src='/3D_ICONS/SETTINGS__GOLD.png' alt='at_sign' width={170} height={170} className='wordPress3D absolute top-[-4.5rem] left-[-2rem] -z-10' />
                 </div>
-                <div className="servicecard hover:scale-110 duration-300 cursor-pointer z-10 backdrop-blur-[10px] bg-white shadow-md flex items-center justify-center shadow-zinc-300 rounded-2xl border border-zinc-300 text-center">
+                <div className="servicecard h-fit hover:scale-110 duration-300 cursor-pointer z-10 backdrop-blur-[10px] bg-white shadow-md flex items-center justify-center shadow-zinc-300 rounded-2xl border border-zinc-300 text-center">
                     <div className="servicecard-overlay backdrop-blur-sm z-0 bg-transparent  rounded-2xl"></div>
                     <Link href='/our-services/graphic-design' className="servicecard-inner z-30 backdrop-blur-sm"> طراحی <br /> گرافیک</Link>
                     <Image src='/3D_ICONS/PAINT__BRUSH.png' alt='brush' width={170} height={170} className='wordPress3D absolute top-[-5rem] left-[-2rem] lg:top-[6rem] lg:left-[-2rem] -z-10' />
                 </div>
-                <div className="servicecard hover:scale-110 duration-300 cursor-pointer z-10 backdrop-blur-[10px] bg-white shadow-md flex items-center justify-center shadow-zinc-300 rounded-2xl border border-zinc-300 text-center">
+                <div className="servicecard h-fit hover:scale-110 duration-300 cursor-pointer z-10 backdrop-blur-[10px] bg-white shadow-md flex items-center justify-center shadow-zinc-300 rounded-2xl border border-zinc-300 text-center">
                     <div className="servicecard-overlay backdrop-blur-sm z-0 bg-transparent  rounded-2xl"></div>
                     <Link href="/our-services/government-learning" className="servicecard-inner z-30 backdrop-blur-sm"> آموزش <br /> کارکنان <br /> دولت</Link>
                     <Image src='/3D_ICONS/NOTEBOOK.png' alt='at_sign' width={170} height={170} className='wordPress3D absolute top-[-4.5rem] left-[-2rem] -z-10' />
--- components/home-components/WebDesignPlans/WordPress.tsx
+++ components/home-components/WebDesignPlans/WordPress.tsx
@@ -32,7 +32,7 @@ const WordPress: NextPage<Props> = ({ }) => {
                     ابزارهای قدرتمند برای بهینه‌سازی موتورهای جستجو (SEO) به راحتی در دسترس است.
                     این مزایا به شما کمک می‌کند تا وب‌سایت کارآمد و حرفه‌ای ایجاد کنید. <span className='tracking-tighter h-fit bg-gradient-to-tr from-[#4866CF] via-blue-700 to-slate-700 text-4xl font-extrabold bg-clip-text text-transparent'>...</span>
                 </div>
-                <button className='bg-gradient-to-tr whitespace-nowrap from-[#4866CF] via-blue-700 to-slate-700 flex px-10 py-3 rounded-lg gap-x-4 items-center justify-between text-white hover:scale-105 duration-300 '>
+                <button className='bg-gradient-to-tr w-[50%] whitespace-nowrap from-[#4866CF] via-blue-700 to-slate-700 flex px-10 py-3 rounded-lg gap-x-4 items-center justify-between text-white hover:scale-105 duration-300 '>
                     توضیحات بیشتر
                     <TiPlus className='icon' />
                 </button>
