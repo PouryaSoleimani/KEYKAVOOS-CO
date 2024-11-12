d213b7ccafb120aeb402d6abcbfdf8a440a8ee9f
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-11-13 00:17:53 +0330

    RESPONSIVE

--- components/home-components/reason.tsx
+++ components/home-components/reason.tsx
@@ -7,7 +7,7 @@ function Reason() {
   return (
     <div className="flex flex-col px-3 lg:px-12" data-aos="fade-up" data-aos-duration="1500">
       <SectionHeader mainTitle="چرا کیکاووس زمان را انتخاب کنیم؟" subTitle="دلایل انتخاب کیکاووس زمان چه چیزهایی است؟" width="24%" />
-      <div className="flex flex-wrap items-center justify-center p-10 gap-10 pt-24 ">
+      <div className="flex flex-wrap items-center justify-center pt-16 gap-6 lg:gap-16 ">
         {ReasonData.map((item) => (
           <div key={item.id}>
             <ReasonCard data={item} />
