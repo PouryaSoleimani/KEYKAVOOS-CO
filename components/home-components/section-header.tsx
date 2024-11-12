267e757ff7ea45a2f2715d9f446585cd06cc5209
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-11-12 23:06:09 +0330

    RESPONSIVE

--- components/home-components/section-header.tsx
+++ components/home-components/section-header.tsx
@@ -4,15 +4,9 @@ type SectionHeaderProps = { mainTitle: string; subTitle: string; width: string; 
 function SectionHeader({ mainTitle, subTitle, width, marginBottom, marginTop, }: SectionHeaderProps) {
   return (
     <div className="relative mt-20 sm:mt-24 mb-10 sm:mb-20 whitespace-normal lg:whitespace-nowrap">
-      <div
-        style={{ position: "absolute", top: "0", left: "0", width: width, height: "100%", border: "none", borderTop: "3px solid", borderImage: "linear-gradient(to left, rgba(72, 102, 207, 1) -5%, rgba(234, 239, 246, 1) 100%) 1", }}
-        className="lg:inline hidden"
-      ></div>
-      <div
-        style={{ position: "absolute", top: "0", right: "0", width: width, height: "100%", border: "none", borderTop: "3px solid", borderImage: "linear-gradient(to left, rgba(234, 239, 246, 1) -5%, rgba(72, 102, 207, 1) 100%) 1", }}
-        className="lg:inline hidden"
-      ></div>
-      <div className="-top-10 left-1/2 absolute bg-transparent text-center -translate-x-1/2 text-xl whitespace-pre-wrap w-full">
+      <div style={{ position: "absolute", top: "0", left: "0", width: width, height: "100%", border: "none", borderTop: "3px solid", borderImage: "linear-gradient(to left, rgba(72, 102, 207, 1) -5%, rgba(234, 239, 246, 1) 100%) 1", }} className="lg:inline hidden" ></div>
+      <div style={{ position: "absolute", top: "0", right: "0", width: width, height: "100%", border: "none", borderTop: "3px solid", borderImage: "linear-gradient(to left, rgba(234, 239, 246, 1) -5%, rgba(72, 102, 207, 1) 100%) 1", }} className="lg:inline hidden"  ></div>
+      <div className="-top-10 px-4 tracking-tighter left-1/2 absolute bg-transparent text-center -translate-x-1/2 text-xl whitespace-normal lg:whitespace-nowrap w-full">
         <p className="font-bold text-[#68707A] sm:text-[32px] tracking-tighter leading-relaxed">
           {mainTitle}
         </p>
