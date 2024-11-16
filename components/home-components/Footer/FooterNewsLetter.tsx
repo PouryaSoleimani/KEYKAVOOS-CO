9199317f66e8551cbcbc951025c592c186f5b174
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-11-16 19:31:18 +0330

    RESPONSIVE

--- components/home-components/Footer/FooterNewsLetter.tsx
+++ components/home-components/Footer/FooterNewsLetter.tsx
@@ -9,6 +9,7 @@ import * as yup from 'yup';
 
 const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,5}$/i;
 
+type Inputs = { name: string, email: string, phonenumber: number | string, textarea?: string | number }
 
 // ^ COMPONENT ===================================================================================================================================================================================
 function FooterNewsLetter() {
@@ -19,48 +20,55 @@ function FooterNewsLetter() {
     .required();
 
   const { register, handleSubmit, formState: { errors }, reset } = useForm({ resolver: yupResolver(schema) });
+
   const [email, setEmail] = useState("");
   const [validEmail, setValidEmail] = useState(false);
   const [emailSuccess, setEmailSuccess] = useState("");
 
-  const handleEmailSubmission = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    try {
-      const { data } = await app.post(
-        "https://keykavoos.liara.run/User/signNewsClub",
-        {
-          email,
-        }
-      );
-      setEmailSuccess("شما با موفقیت عضو شدید.");
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data); reset()
+  }
 
-  useEffect(() => {
-    1
-    const validMail = emailRegex.test(email);
-    setValidEmail(validMail);
-  }, [email]);
+  // const handleEmailSubmission = async (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   try {
+  //     const { data } = await app.post(
+  //       "https://keykavoos.liara.run/User/signNewsClub",
+  //       {
+  //         email,
+  //       }
+  //     );
+  //     setEmailSuccess("شما با موفقیت عضو شدید.");
+  //     console.log(data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   1
+  //   const validMail = emailRegex.test(email);
+  //   setValidEmail(validMail);
+  // }, [email]);
 
   return (
     <div className="flex flex-col gap-2 items-stretch justify-center lg:justify-start lg:gap-6">
       <p className={`${styles["footer-section-title"]} tracking-tighter font-normal pr-1`}>مشترک شوید تا آخرین اخبار را دریافت کنید.</p>
-      <form className="flex gap-2 flex-col lg:flex-row" onSubmit={(e) => handleEmailSubmission(e)}  >
+      <form className="flex gap-2 flex-col lg:flex-row" onSubmit={handleSubmit(onSubmit)} >
         <div className="w-full">
           <input
             placeholder="ایمیل خودرا وارد کنید..."
             type="email"
-            value={email}
+            // value={email}
+            {...register('email', { required: true })}
             className="bg-[#F8FAFC] text-lg placeholder:text-sm rounded-lg w-full py-2 px-3 outline-none border-none"
             aria-describedby="emailnote"
-            onChange={(e) => setEmail(e.target.value)}
+            // onChange={(e) => setEmail(e.target.value)}
           />
-          <p id="emailnote" className={email && !validEmail ? "bg-red-300 text-red-900 text-[0.75rem] p-[0.5rem] rounded-md relative -bottom-[10px] mb-2 lg:mb-0" : "absolute -left-[9999px]"}  >
+            {errors.email && <p className='text-red-900 absolute top-36 left-[78%] tracking-tighter text-sm'>ایمیل وارد شده صحیح نمی باشد</p>}
+          {/* <p id="emailnote" className={email && !validEmail ? "bg-red-300 text-red-900 text-[0.75rem] p-[0.5rem] rounded-md relative -bottom-[10px] mb-2 lg:mb-0" : "absolute -left-[9999px]"}  >
             ایمیل معتبر نیست.
-          </p>
+          </p> */}
         </div>
         <button className="flex gap-1 p-2 text-white bg-[#4866CF] rounded-lg items-center whitespace-nowrap lg:w-[160px] w-[120px] lg:h-[45px] hover:bg-blue-800 duration-300">
           <span className="tracking-tighter px-1 flex items-center gap-1">مشترک شوید
