8f01615a0206a196de0ce58112b470699510a951
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-11-17 20:30:54 +0330

    DEBUGGING

--- app/contact-us/components/contact-heading.tsx
+++ app/contact-us/components/contact-heading.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const ContactHeading = () => {
   return (
-    <div className="grid grid-cols-1 justify-center items-center text-center w-[80%] mx-auto bg-contactBg bg-no-repeat bg-center bg-[length:100%_100%] mt-[3%]">
+    <div className="grid grid-cols-1 justify-center items-center text-center w-[80%] mx-auto bg-contactBg bg-no-repeat bg-center bg-[length:100%_100%] mt-[3%] font-YekanBakh">
       <p className="text-[rgb(72,102,207)] text-[33px] font-bold lg:text-[110px]">تماس با ما</p>
-      <p className="xl:text-[61px] font-bold lg:max-xl:text-[45px] text-[20px] w-[90%] mx-auto">تماس با ما راهی سریع امن و عالی هست اگه با ما کار دارید تماس بگیرید</p>
+      <p className="xl:text-[32px] font-semibold tracking-tight lg:max-xl:text-[45px] text-[20px] w-[90%] mx-auto">تماس با ما راهی سریع امن و عالی هست اگه با ما کار دارید تماس بگیرید</p>
     </div>
   );
 };
