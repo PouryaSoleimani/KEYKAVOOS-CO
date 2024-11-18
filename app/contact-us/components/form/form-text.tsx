c31298d5fec7b81d0d09e2669090ed6f81695011
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-11-18 22:47:31 +0330

    RESPONSIVE

--- app/contact-us/components/form/form-text.tsx
+++ app/contact-us/components/form/form-text.tsx
@@ -1,10 +1,8 @@
 const FormText = () => {
   return (
-    <div className="flex flex-wrap gap-2 text-center md:text-right justify-center lg:flex-col">
-      <p className="text-[40px] font-semibold ">
-        تماس با پشتیبانی
-      </p>
-      <p className="lg:text-[30px] font-normal text-[20px] 8xl:text-[18px] whitespace-nowrap">
+    <div className="flex flex-wrap gap-3 my-4 text-center md:text-center justify-center lg:flex-col">
+      <p className="text-[30px] font-bold text-center font-YekanBakh tracking-tight">  تماس با پشتیبانی  </p>
+      <p className="lg:text-[24px] tracking-tighter text-zinc-700 font-normal text-[20px] 8xl:text-[18px] whitespace-nowrap">
         جواب دهی در سریع ترین زمان ممکن
       </p>
     </div>
--- app/contact-us/components/options/options-card.tsx
+++ app/contact-us/components/options/options-card.tsx
@@ -3,7 +3,7 @@ import React from "react";
 type OptionsCardProps = { src: string; head: string; info: string; };
 const OptionsCard = ({ src, head, info }: OptionsCardProps) => {
   return (
-    <div    className="w-full h-full py-[15%] px-[1%] rounded-[50px] overflow-hidden flex flex-col items-center justify-center mx-auto"    style={{ boxShadow: "0px 17px 44px -1px rgba(0, 0, 0, 0.25)" }}  >
+    <div className="w-full h-full py-[10%] px-[1%] rounded-[30px] overflow-hidden flex flex-col items-center justify-center mx-auto hover:scale-105 duration-500" style={{ boxShadow: "0px 17px 44px -1px rgba(0, 0, 0, 0.25)" }}  >
       {/* Logo */}
       <div className="w-[150px] h-[150px] bg-gradient-to-b from-zinc-300 to-indigo-400 rounded-full flex justify-center items-center">
         <Image src={src} className="w-[60px] h-[60px]" alt='pic' width={50} height={50} />
