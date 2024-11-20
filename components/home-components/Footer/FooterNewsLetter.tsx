f3f007fc53f84653b105a263608e0130422d93e6
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-11-20 10:56:12 +0330

    REFACTOR

--- components/home-components/Footer/FooterNewsLetter.tsx
+++ components/home-components/Footer/FooterNewsLetter.tsx
@@ -41,11 +41,11 @@ function FooterNewsLetter() {
         <form className="flex gap-2 flex-col lg:flex-row" onSubmit={handleSubmit(onSubmit)} >
           <div className="w-full">
             <input className="bg-[#F8FAFC] text-lg placeholder:text-sm rounded-lg w-full py-2 px-3 outline-none border-none" placeholder="ایمیل خودرا وارد کنید..." type="email" {...register('email', { required: true })} aria-describedby="emailnote" />
-            {errors.email && <p className='text-red-900 bg-red-200/40 font-thin px-2 py-0.5 rounded-md absolute top-36 right-[3%] tracking-tighter text-sm'>ایمیل وارد شده صحیح نمی باشد</p>}
+            {errors.email && <p className='text-red-900 bg-red-200/40 font-thin px-2 py-0.5 rounded-md absolute top-36 right-[2.5%] tracking-tighter text-sm'>ایمیل وارد شده صحیح نمی باشد</p>}
           </div>
           <button type="submit" className="flex gap-1 p-2 text-white bg-[#4866CF] rounded-lg items-center whitespace-nowrap lg:w-[160px] w-[120px] lg:h-[45px] hover:bg-blue-800 duration-300">
-            <span className="tracking-tighter px-1 flex items-center gap-1">مشترک شوید
-              <Image width={3} height={3} alt="footer_pic" src="/white-arrow.svg" className="w-3 h-3 pl-1" />
+            <span className="tracking-tighter px-1 flex items-center gap-2 leading-5">مشترک شوید
+              <Image width={3} height={3} alt="footer_pic" src="/white-arrow.svg" className="w-3 h-3 pl-1 mb-0.5" />
             </span>
           </button>
         </form>
