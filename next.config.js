e51e4bc9a21bc9952b5b32ccb71f79d5c2d8a05c
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-12-03 12:57:32 +0330

    config error

--- next.config.js
+++ next.config.js
@@ -1,19 +1,13 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: { ignoreBuildErrors: true, },
   eslint: { ignoreDuringBuilds: true, },
   productionBrowserSourceMaps: true,
-  images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "back.keykavoos.co", port: "", pathname: "back.keykavoos.co/storage", },
-      { protocol: "http", port: "3000", hostname: "***" },
-    ],
-  },
+  images: { domains: ["localhost", "back.keykavoos.co/storage", "localhost:3000",], },
+  images: { remotePatterns: [{ protocol: "https", hostname: "**", }, { protocol: "http", port: "3000", hostname: "***" }], },
 };
 const withBundleAnalyzer = require('@next/bundle-analyzer')({ enabled: process.env.ANALYZE === 'true', })
-
 module.exports = { productionSourceMaps: true, };
 module.exports = nextConfig;
 module.exports = withBundleAnalyzer(nextConfig)
-module.exports = { experimental: { turbo: { resolveExtensions: ['.mdx', '.tsx', '.ts', '.jsx', '.js', '.mjs', '.json',], }, }, }
-module.exports = { images: { domains: ['back.keykavoos.co'], }, }
