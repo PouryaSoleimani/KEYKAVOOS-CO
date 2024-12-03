6a4a025315a285af6c541e38f05e517e7f20a61d
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-12-03 13:03:44 +0330

    config error

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: { ignoreBuildErrors: true, },
   eslint: { ignoreDuringBuilds: true, },
   productionBrowserSourceMaps: true,
-  images: { domains: ["localhost", "back.keykavoos.co/storage", "localhost:3000",], },
-  images: { remotePatterns: [{ protocol: 'https', hostname: 'back.keykavoos.co', },] },
 };
 const withBundleAnalyzer = require('@next/bundle-analyzer')({ enabled: process.env.ANALYZE === 'true', })
-module.exports = { productionSourceMaps: true, nextConfig; };
+module.exports = { productionSourceMaps: true, nextConfig };
 module.exports = withBundleAnalyzer(nextConfig)
 module.exports = {
   images: { remotePatterns: [{ protocol: 'https', hostname: 'back.keykavoos.co' }], },
