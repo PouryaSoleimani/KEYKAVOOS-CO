f32f37f20b674e59d0dd935c225259af5d80d77b
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-12-27 16:19:38 +0330

    ADMIN PANEL

--- app/panel/admin/consultations/consult-detail/page.tsx
+++ app/panel/admin/consultations/consult-detail/page.tsx
@@ -15,7 +15,7 @@ function ConsultDetail() {
   const { token } = useSelector((state: any) => state.userData);
   const params = useSearchParams();
   const id = params.get("id");
-  const [consultationDetail, setConsultationDetail] = useState({ title: "", description: "", date: "", user_id: "" });
+  const [consultationDetail, setConsultationDetail] = useState<any>({ title: "", description: "", date: "", user_id: "" });
   const [consultDetailStatus, setConsultDetailStatus] = useState({ loading: false, erorr: "", });
 
   useEffect(() => { getConsultationDetail(token, id, setConsultationDetail, setConsultDetailStatus); }, []);
@@ -30,7 +30,8 @@ function ConsultDetail() {
       </div>
 
       <div className="bg-white shadow mx-auto rounded-lg w-full p-[3%] text-center grid grid-cols-1 gap-5">
-        <div className="grid grid-cols-4">
+        <div className="grid grid-cols-5">
+          <div>ID</div>
           <div>تاریخ درخواست مشاوره</div>
           <div>عنوان مشاوره</div>
           <div> USER_ID </div>
@@ -43,7 +44,8 @@ function ConsultDetail() {
         ) : consultDetailStatus.erorr ? (
           <NotFound text={`${consultDetailStatus.erorr}`} />
         ) : (
-          <div className="grid grid-cols-4 py-4 bg-[#EAEFF6] rounded-[4px]">
+          <div className="grid grid-cols-5 py-4 bg-[#EAEFF6] rounded-[4px]">
+            <p>{consultationDetail.id}</p>
             <p>{consultationDetail.date ? moment(consultationDetail.date, "YYYY-MM-DDTHH:mm:ss.SSSZ").format("jYYYY/jM/jD") : "-"}</p>
             <p>{consultationDetail.title}</p>
             <p>{consultationDetail.user_id ? consultationDetail.user_id : " --- "}</p>
