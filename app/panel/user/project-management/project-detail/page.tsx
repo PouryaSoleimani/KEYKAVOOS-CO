7bb079bdb3980dfab99118001255ac7efc26ad6c
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-01-02 17:10:57 +0330

    ADMIN PANEL

--- app/panel/user/project-management/project-detail/page.tsx
+++ app/panel/user/project-management/project-detail/page.tsx
@@ -83,6 +83,20 @@ function ProjectDetail() {
     //   </div>
     // </div>
     <div>
+      {/* //^ PROGRESS BAR  */}
+      <ul className="steps mx-auto my-16 w-full" dir="ltr">
+        <li className="step whitespace-nowrap text-sm tracking-tighter  step-primary">تکمیل سفارش</li>
+        <li className="step whitespace-nowrap text-sm tracking-tighter  step-primary">واریز پیش پرداخت</li>
+        <li className="step whitespace-nowrap text-sm tracking-tighter  step-primary">بررسی نیاز های پروژه</li>
+        <li className="step whitespace-nowrap text-sm tracking-tighter  step-primary">کدنویسی بک اند</li>
+        <li className="step whitespace-nowrap text-sm tracking-tighter  step-primary">ساخت دیتابیس</li>
+        <li className="step whitespace-nowrap text-sm tracking-tighter  step-primary">طراحی رابط کاربری</li>
+        <li className="step whitespace-nowrap text-sm tracking-tighter  step-primary">کدنویسی فرانت اند</li>
+        <li className="step whitespace-nowrap text-sm tracking-tighter  step-primary">ایجاد درگاه ها و لینک ها</li>
+        <li className="step whitespace-nowrap text-sm tracking-tighter  step-primary">کنترل کیفی نهایی</li>
+        <li className="step whitespace-nowrap text-sm tracking-tighter  step-primary">تحویل وب اپلیکیشن</li>
+      </ul>
+      
       <div className="w-[210px] z-10">
         <div className="bg-[#4866CE] text-white rounded-t-lg relative right-1 top-1 py-2 px-2 flex justify-start items-center gap-2">
           <span>شماره درخواست:</span>
