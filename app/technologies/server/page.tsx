8c0a72e9d099c4f4a7eee26346720e3ccc3efb4f
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-01-03 14:01:04 +0330

    DATABASE PAGE

--- app/technologies/server/page.tsx
+++ app/technologies/server/page.tsx
@@ -1,8 +1,35 @@
+import GovernmentLearningCarousel3 from '@/components/GOVERNMENT__LEARNING__COMPONENTS/GovernmentLearningCarousel3'
+import Footer from '@/components/home-components/Footer/Footer'
+import Nav from '@/components/home-components/nav'
 import React from 'react'
 
 const ServerPage = () => {
   return (
-    <div>ServerPage</div>
+    <div className='web-container h-auto overflow-x-hidden'>
+      <Nav />
+      {/* HEADER */}
+      <div className='py-4 animate__animated animate__backInDown animate__slow'>
+        <h1 className='BLOG_HEADER_TEXT text-center font-black bg-gradient-to-tr from-blue-300 via-[#4866CF] to-blue-900 bg-clip-text text-3xl lg:text-5xl py-4 text-transparent BLOG_HEADER_TEXT animate__animated animate__heartBeat animate__infinite'>ساخت و توسعه دیتابیس </h1>
+        <h2 className='text-center font-extralight text-md text-zinc-700 bg-zinc-300 py-2 px-4 rounded-xl w-fit mx-auto'>با استفاده از پرظرفیت ترین و سریع ترین ابزارهای آنلاین</h2>
+      </div>
+      <video src="/GRAPHIC__DESIGN/1.mp4" loop autoPlay height={100} className='h-80 w-fit mx-auto my-10 rounded-3xl' />
+      <div className='flex flex-col items-center justify-center gap-16 pb-32  h-auto' data-aos="fade-up" data-aos-duration="1000">
+        <h2 className="text-2xl">تکنولوژی ها و ابزار های مورد استفاده در ساخت و توسعه دیتابیس</h2>
+        <GovernmentLearningCarousel3 />
+      </div>
+      <div className='w-[90%] py-10 mx-auto  rounded-xl backdrop-blur-sm  p-4 mt-1 mb-16  text-justify bg-transparent leading-10 border-blue-800 border-4'>
+        <p dir='rtl' className='whitespace-pre-wrap px-4'>
+          ساخت و توسعه دیتابیس برای وب‌سایت فرآیند طراحی، پیاده‌سازی و مدیریت سیستم‌های ذخیره‌سازی داده‌ها است که از آن برای ذخیره، بازیابی، و پردازش اطلاعات مربوط به وب‌سایت یا برنامه استفاده می‌شود. دیتابیس‌ها به عنوان یکی از ارکان اساسی در هر وب‌سایت یا برنامه کاربردی به شمار می‌آیند و اطلاعات مختلف از جمله داده‌های کاربران، محصولات، سفارشات، مطالب، نظرات و سایر اطلاعات مهم را ذخیره می‌کنند.
+          <br />
+          فرآیند توسعه دیتابیس شامل طراحی ساختار پایگاه داده (Schema)، تعیین جداول، روابط بین جداول، و همچنین انتخاب نوع دیتابیس مناسب است. دیتابیس‌ها معمولاً به دو دسته اصلی تقسیم می‌شوند: دیتابیس‌های رابطه‌ای (Relational) مانند MySQL، PostgreSQL، و SQLite که از جداول و روابط برای ذخیره‌سازی داده‌ها استفاده می‌کنند، و دیتابیس‌های غیررابطه‌ای (NoSQL) مانند MongoDB، Cassandra و Firebase که بیشتر برای داده‌های بزرگ و بدون ساختار مناسب هستند.
+          <br />
+          در فرآیند ساخت دیتابیس، مهم‌ترین نکات شامل اطمینان از کارایی بالا، امنیت داده‌ها، مقیاس‌پذیری (Scalability) و قابلیت دسترسی سریع به اطلاعات است. همچنین، برای اطمینان از عملکرد صحیح سیستم، توسعه‌دهندگان باید از روش‌های بهینه‌سازی کوئری‌ها، پشتیبانی از نسخه‌های مختلف دیتابیس و مدیریت تراکنش‌ها استفاده کنند.
+          <br />
+          توسعه‌دهندگان دیتابیس باید علاوه بر طراحی بهینه ساختار داده‌ها، امنیت دیتابیس را نیز در نظر بگیرند تا اطلاعات حساس کاربران یا سازمان به‌خوبی محافظت شود. استفاده از روش‌های مختلف مانند رمزنگاری، مدیریت دسترسی و مانیتورینگ مداوم برای جلوگیری از نفوذ یا دسترسی‌های غیرمجاز از نکات ضروری در توسعه دیتابیس است. به‌طورکلی، توسعه دیتابیس وب‌سایت یک بخش حیاتی است که بر عملکرد و امنیت کل سیستم تأثیرگذار است.
+        </p>
+      </div>
+      <Footer />
+    </div>
   )
 }
 
